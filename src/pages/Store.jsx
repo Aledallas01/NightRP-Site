29fb3bd5e139287602ace60a5a61085543e1586c
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-05-04 21:21:08 +0200

    Modify Store.jsx

---
+++ src/pages/Store.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+export default function Store() {
+  return (
+<>
+    <meta charSet="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <title>Store | NightRP Minecraft Server</title>
+    {/* Font Awesome (aggiungi QUI) */}
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    />
+    <link rel="stylesheet" href="../CSS/style.css" />
+    {/* HEADER */}
+    <div id="header-placeholder" />
+    {/* MAIN */}
+    <main>
+      {/* Hero */}
+      <section className="hero" id="hero">
+        <h1>
+          <i className="fas fa-th-list" /> Coming Soon
+        </h1>
+        <p>
+          Lo Store non è ancora disponibile! <br /> Torna più tardi per scoprire le ultime novità e gli aggiornamenti.
+        </p>
+      </section>
+    </main>
+    {/* FOOTER */}
+    <div id="footer-placeholder" />
+</>
+
+  );
+}
