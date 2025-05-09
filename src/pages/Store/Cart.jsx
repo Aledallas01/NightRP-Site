525e881fbc4a4b03a248d61feaf81a1bc0afecea
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-05-09 10:57:24 +0200

    Fix Store

--- src/pages/Store/Cart.jsx
+++ src/pages/Store/Cart.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import CartContext from '../../../contexts/CartContext';
+import CartContext from '../../contexts/CartContext';
 import { Link } from 'react-router-dom';
 
 export default function Cart() {
