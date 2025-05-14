a02ec5addcc9b01adb359afb7dc0a4f0b0b48a99
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-05-14 20:46:11 +0200

    Fix

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,6 +1,6 @@
 // src/components/CheckoutForm.jsx
 import React, { useState, useContext } from 'react';
-import CartContext from '../contexts/CartContext';
+import { CartContext } from '../contexts/CartContext';
 import { useNavigate } from 'react-router-dom';
 
 export default function CheckoutForm() {
