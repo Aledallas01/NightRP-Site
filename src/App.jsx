a50c18452e9fc6ffc29d222ff8a3faf005513d5e
Aledallas01 <147440074+Aledallas01@users.noreply.github.com>
2025-05-14 18:03:20 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import Home from './pages/Home.jsx';
@@ -17,30 +16,27 @@ import Cart from './pages/Cart.jsx';
 import Checkout from './pages/Checkout.jsx';
 import OrderConfirmation from './pages/OrderConfirmation.jsx';
 
-
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/games" element={<Games />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/skyblock" element={<Skyblock />} />
-            <Route path="/kitpvp" element={<KitPvP />} />
-            <Route path="/prison" element={<Prison />} />
-            <Route path="/roleplay" element={<Roleplay />} />
-            <Route path="/forum" element={<Forum />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/order-confirmation" element={<OrderConfirmation />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
+      <Navbar />
+      <main className="flex-grow">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skyblock" element={<Skyblock />} />
+          <Route path="/kitpvp" element={<KitPvP />} />
+          <Route path="/prison" element={<Prison />} />
+          <Route path="/roleplay" element={<Roleplay />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 }
